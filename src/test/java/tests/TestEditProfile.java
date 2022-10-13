339ac681b6a07a2eb4ebabe13d78264a2fda04be
aleksandrazujovic <aleksandrazujovic997@gmail.com>
2022-10-13 14:03:31 +0200

    Final commit

--- src/test/java/tests/TestEditProfile.java
+++ src/test/java/tests/TestEditProfile.java
@@ -19,7 +19,7 @@ public class TestEditProfile extends BaseTest {
 
         String name = faker.name().fullName();
         String phone = faker.phoneNumber().phoneNumber();
-        String city = faker.address().city();
+        String city = "Bucaramanga";
         String country = faker.country().name();
         String twitter = "http://" + faker.internet().domainName();
         String github = "http://" + faker.internet().domainName();
@@ -28,12 +28,14 @@ public class TestEditProfile extends BaseTest {
 
         String actualName = getEditProfile().getName().getAttribute("value");
         String actualPhone = getEditProfile().getPhone().getAttribute("value");
+        String actualCity = getEditProfile().getCity().getAttribute("value");
         String actualCountry = getEditProfile().getCountry().getAttribute("value");
         String actualTwitter = getEditProfile().getTwitter().getAttribute("value");
         String actualGithub = getEditProfile().getGitHub().getAttribute("value");
 
         Assert.assertEquals(name,actualName);
         Assert.assertEquals(phone,actualPhone);
+        Assert.assertEquals(city, actualCity);
         Assert.assertEquals(country,actualCountry);
         Assert.assertEquals(twitter,actualTwitter);
         Assert.assertEquals(github,actualGithub);
